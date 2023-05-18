230530a93eb37047656dc1364807f083a438337b
David Domínguez Blanco <60604817+DavidDominguez2@users.noreply.github.com>
2023-05-18 20:08:28 +0200

    Add project files.

---
+++ MvcCoreContentModerator/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreContentModerator.Models;
+using MvcCoreContentModerator.Services;
+using System.Diagnostics;
+
+namespace MvcCoreContentModerator.Controllers {
+    public class HomeController : Controller {
+
+        private ServiceContentModerator contentModerator;
+
+        public HomeController(ServiceContentModerator contentModerator) {
+            this.contentModerator = contentModerator;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string text) {
+            var result = this.contentModerator.ModerateText(text);
+            ViewData["result"] = result;
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MvcCoreContentModerator/Services/ServiceContentModerator.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure.CognitiveServices.ContentModerator;
+using Microsoft.Azure.CognitiveServices.ContentModerator.Models;
+using System.Text;
+
+namespace MvcCoreContentModerator.Services {
+    public class ServiceContentModerator {
+
+        private readonly ContentModeratorClient client;
+
+        public ServiceContentModerator(IConfiguration configuration) {
+            // Endpoint
+            string key = configuration.GetValue<string>("AzureKeys:KeyModeration");
+            string endpoint = configuration.GetValue<string>("AzureKeys:EndpointModeration");
+
+            this.client = this.Authenticate(key, endpoint);
+        }
+
+        private ContentModeratorClient Authenticate(string key, string endpoint) {
+            ContentModeratorClient client = new ContentModeratorClient(new ApiKeyServiceClientCredentials(key));
+            client.Endpoint = endpoint;
+
+            return client;
+        }
+
+        public Screen ModerateText(string text, string lang = "eng") {
+            // Create a Content Moderator client and evaluate the text.
+            using (this.client) {
+                return
+                   this.client.TextModeration.ScreenText("text/plain", new MemoryStream(Encoding.UTF8.GetBytes(text)), lang, true, true, null, true);
+            }
+        }
+
+
+
+    }
+}
